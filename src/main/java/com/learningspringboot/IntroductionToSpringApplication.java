a664f94ed00e70f94ea6420cd7a1f1b04b653b30
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2023-12-15 20:50:08 +0530

    Introduction to Spring Boot - Java Springboot Dev Batch

---
+++ src/main/java/com/learningspringboot/IntroductionToSpringApplication.java
@@ -0,0 +1,13 @@
+package com.learningspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learningspringboot/IntroductionToSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
