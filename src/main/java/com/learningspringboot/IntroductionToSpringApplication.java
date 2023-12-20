6a77c95fc16cd3cf8cead352990e661789781d09
SiddharthMishra28 <connectwithsiddharthm@gmail.com>
2023-12-20 18:41:11 +0530

    Spring Boot Api development - Spring Boot Dev Batch

--- src/main/java/com/learningspringboot/IntroductionToSpringApplication.java
+++ src/main/java/com/learningspringboot/IntroductionToSpringApplication.java
@@ -2,11 +2,11 @@ package com.learningspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class IntroductionToSpringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(IntroductionToSpringApplication.class, args);
 	}
